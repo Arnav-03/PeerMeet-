b5007fe8b81b1c1a1c3821b893666cd80178e102
Arnav-03 <arnavarora0003@gmail.com>
2024-03-09 22:49:45 +0530

    frontend

--- src/RoomChoice.jsx
+++ src/RoomChoice.jsx
@@ -33,11 +33,11 @@ const RoomChoice = () => {
       <div className='text-[#ffffff] text-6xl md:text-7xl lg:text-8xl m-1 mt-12 mb-6 ' style={style} >
         PeerMeet</div>
 
-      <div className="flex flex-col md:flex-row">
+      <div className="flex flex-col md:flex-row lg:gap-40">
 
-        <div className="h-[300px] lg:h-[350px] lg:w-[400px] w-[350px] border-2 rounded-3xl border-[#abb8c0] text-center justify-center items-center flex flex-col m-2 p-5 mb-7">
+        <div className="h-[300px] lg:h-[300px] lg:w-[400px] w-[350px] border-2 rounded-3xl border-[#abb8c0] text-center justify-center items-center flex flex-col m-2 p-5 mb-7">
 
-          <div className="bg-[#abb8c0] mt-[-80px] lg:mt-[-130px]  rounded-full border-2 border-[#abb8c0] max-w-20 max-h-20 ">
+          <div className="bg-[#abb8c0] mt-[-80px] lg:mt-[-90px]  rounded-full border-2 border-[#abb8c0] max-w-20 max-h-20 ">
             <img className='h-20 w-20 p-3' src={p2proom} alt="" srcset="" />
           </div>
 
@@ -50,8 +50,8 @@ const RoomChoice = () => {
 
         </div>
 
-        <div className="h-[300px] lg:h-[350px] lg:w-[400px] w-[350px] border-2 rounded-3xl border-[#abb8c0] text-center justify-center items-center flex flex-col m-2 p-5">
-          <div className="bg-[#abb8c0] mt-[-80px] rounded-full border-2 border-[#abb8c0] max-w-20 max-h-20 lg:mt-[-130px] ">
+        <div className="h-[300px] lg:h-[300px] lg:w-[400px] w-[350px] border-2 rounded-3xl border-[#abb8c0] text-center justify-center items-center flex flex-col m-2 p-5">
+          <div className="bg-[#abb8c0] mt-[-80px] rounded-full border-2 border-[#abb8c0] max-w-20 max-h-20 lg:mt-[-90px] ">
             <img className='h-20 w-20 p-2 ' src={grpproom} alt="" srcset="" />
           </div>
           <div style={titlestyle} className="text-2xl text-[#3da0ad] m-3">Group Space</div>
