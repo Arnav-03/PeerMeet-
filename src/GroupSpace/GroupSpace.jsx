9d310b0ef661da97fe1642c0ac08b53774032767
Arnav-03 <arnavarora0003@gmail.com>
2024-03-10 02:03:08 +0530

    choice

--- src/GroupSpace/GroupSpace.jsx
+++ src/GroupSpace/GroupSpace.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 const GroupSpace = () => {
   return (
-    <div className='text-white'>
-      group space
+    <div className='h-screen text-2xl flex flex-col justify-center items-center text-white  uppercase'>
+      <div className="">group space</div>
+      <div className="text-red-600 font-bold uppercase">work under progress</div>
     </div>
   )
 }
